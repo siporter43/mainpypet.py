c9fdd187c10b7f8982bacb8a48fdc519bb6e8132
sean porter <siporter43@gmail.com>
2020-10-27 22:37:54 +0000

    a bunch

--- apis.py
+++ apis.py
@@ -1,6 +1,9 @@
 from pprint import pprint
 import requests 
 
+from private import LAT, LNG, OPENUV_KEY
+
+
 def request_demo():
     #Explore how web requests work
     url = "https://raw.githubusercontent.com/alissa-huskey/python-class/master/hello.txt"
@@ -52,4 +55,62 @@ def request_location():
     data = response.json()
     print(f"We are at {data['latitude']} , {data['longitude']} right now")
 
-request_location()
+#request_location()
+
+
+#curl "https://fourtonfish.com/hellosalut/?lang=de"
+#we're about to call for this to output a greeting in german
+def request_hello():
+  url = "https://fourtonfish.com/hellosalut/?lang=de"
+  response = requests.get(url, params={'lang': 'de'})
+  data = response.json()
+  print(data['hello'])
+
+request_hello()
+#below requests activities
+def request_boredapi():
+  url = "https://www.boredapi.com/api/activity"
+  response = requests.get(url, params={'participants' : '2'})
+  data = response.json()
+  print(data['activity'])
+request_boredapi()
+
+def request_insult():
+  url = "https://evilinsult.com/generate_insult.php"
+  response = requests.get(url, params={'lang': 'en' , 'type' : 'json'})
+  data = response.json()
+  print(data['insult'])
+request_insult()
+
+def request_agify():
+  url = "https://api.agify.io"
+  response = requests.get(url, params={'name': 'Dan'})
+  data = response.json()
+  print(data['age'])
+request_agify()
+
+
+def request_uv():
+    #here's hoping this prints UV and OZone info for today
+    response = requests.get(
+        "https://api.openuv.io/api/v1/uv",
+        params = {'lat': LAT, 'lng': LNG},
+        headers= {'x-access-token':OPENUV_KEY}
+    )
+    data = response.json()
+    print("UV Index:", data['result']['uv'])
+    print("Ozone:", data['result']['ozone'])
+request_uv()
+
+def request_shakespeare():
+    url = "https://shakespeare1.p.rapidapi.com/shakespeare/generate/insult"
+    response = requests.get(url, params={"limit": "1"}, 
+    headers= {'rapidAPI_KEY':"7c40183e4cmsh905dcd68bafea67p1b8572jsndedc57c1e54f"})
+    data = response.json()
+    if not response.ok:
+      print("There was a problem with your request!")
+      print(response.status_code, response.reason)
+      pprint(response.headers)
+      return
+    print(data['quote'])
+request_shakespeare()
---
+++ private.py
@@ -0,0 +1,8 @@
+_all_= ["LAT", "LNG"]
+
+LAT = "34.06"       
+LNG = "-118.26"
+
+OPENUV_KEY = "5561571f53ee24290966dc868b260ae7"
+
+rapidAPI_KEY= "7c40183e4cmsh905dcd68bafea67p1b8572jsndedc57c1e54f"
