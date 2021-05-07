a83718891a2705c9c2a693c3cd42d15aeced3338
siporter43 <67347341+siporter43@users.noreply.github.com>
2021-05-07 14:39:41 -0700

    We did some creation of directories, files, and et
    Co-authored-by: Alissa Huskey <alissa.huskey@gmail.com>

--- project_outrun/May3.py
+++ project_outrun/May3.py
@@ -7,23 +7,30 @@ contains something related to that person.
 
 1.[x] Make a list of people's names that you'd like to keep track of information about.
 2. [x]Create a new directory called something like people if it does not exist.
-3. [ ]Iterate over the list of people.
+3. [x]Iterate over the list of people.
     -[x] Create a blank file called {name}.txt in the people directory if it doesn't exist.
-    - [ ]Write something to the file about that person
+    - [x]Write something to the file about that person
     
 4. Write something different for each person in the list
-    [ ] Change the list to a dict where the key is the name of the person
+    [x] Change the list to a dict where the key is the name of the person
         and the value is a gift idea.
-    [ ] Change your for loop to get the name and gift of each item
-        in the dict using the .items() method
-    [ ] Write a second line to the file with their gift idea
+    [x] In the for loop, get the name of the person from the path object.
+    [x] Get the gift idea from the dictionary.
+    [x] Write a second line to the file with their gift idea
 """
 
 from pathlib import Path
 
 """variables"""
 
-commie_list = ["Kitty Pryde", "Scott Summers", "Jean Grey", "Orroro Monroe", "Kurt Wagner", "etc"]
+commie_list = {
+    "Kitty Pryde": "Pirate Queen",
+    "Scott Summers": "Boy Scout",
+    "Jean Grey": "Head Hunter", 
+    "Orroro Monroe": "Storm Lady",
+    "Kurt Wagner": "Disappearer",
+    "etc": "Bling Blong"
+}
 
 
 """fncns"""
@@ -54,11 +61,15 @@ def x_write():
     print("=" * 50)
     path = Path("data") / "Xman_Xmas"
     for file_path in path.iterdir():
+        stem_name = file_path.stem
+        wording = stem_name.replace("_", " ")
         print(f"writing to {file_path}")
         fh = open(f"{file_path}", "w")
-        fh.write("Create More Mutants")
+        fh.write(f"{commie_list[wording]}\n")
+        fh.write("\nCreate More Mutants\n")
+        
         fh.close()
-    print("harm no man")
+    print("Harm no man")
     print("=" * 50)
 
 """script runner"""
